f5ed2af780b65d113a001ca92527dd5b7e4717cc
charlene04 <charlesugbana04@gmail.com>
2021-01-23 04:23:46 +0100

    new

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public/')))
 
 
-app.use(fileUpload())
+app.use(fileUpload({debug: true}))
 
 app.use(cookieParser());
 app.use(
